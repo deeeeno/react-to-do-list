9c8fbefb68bb614136617ba35b498fafcd7a18c4
deeeeno <ksh0512dog@gmail.com>
2022-01-10 23:54:53 +0900

    push project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [toDo,setToDo] = useState('');
+  const [toDos,setToDos] = useState([]);
+  const onChange = (event)=>setToDo(event.target.value);
+  const onSubmit = (event)=> {
+    event.preventDefault();
+    if(toDo.length === 0) return;
+
+    setToDos((currentToDos => [toDo,...currentToDos]));
+    setToDo("");
+  }
+  //리스트 JSX를 출력할땐 key props가 필수.
+  //왜냐 각 jSX를 다른 Item으로 보기 때문이다.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My To Dos({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input value={toDo} onChange={onChange} type="text" placeholder="Write your to do..."></input>
+        <button>Add To Do</button>
+      </form>
+      <ul>
+      {toDos.map((ele,idx)=><li key={idx}>{ele}</li>)}
+      </ul>
     </div>
   );
 }
